260ae3301b2767ee89fdebfcba52604277a0e341
Charity-77 <charitykamogelo07@gmail.com>
2025-09-03 02:12:18 +0200

    comments

--- camel/camel.py
+++ camel/camel.py
@@ -1,11 +1,11 @@
-#before
+#define a function that will input a camelCase , the outpu an underscodre before the capiital letter
 def main():
-    result = ""
-    camelCase = input("camelCase: ")
-    for char in camelCase:
-        if char.isupper():
-            result += "_" + char.lower()
+    result = "" #define an empty variable
+    camelCase = input("camelCase: ") 
+    for char in camelCase: # calling out characters in the input.It fetches all the characters
+        if char.isupper(): # if the character(letter) is in capital letter
+            result += "_" + char.lower() # reassign the variable by underscore before the uppercase letter to then lower the letter
         else:
-            result +=char
+            result +=char # otherwise assign the lower variable 
     print(result)
 main()
