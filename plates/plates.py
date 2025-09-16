1931440ec656f43548fda9924be38d3c7e45c563
Charity-77 <charitykamogelo07@gmail.com>
2025-09-16 04:47:05 +0200

    the first draft of plates

---
+++ plates/plates.py
@@ -0,0 +1,26 @@
+#input ER. if is_valid(ER),char
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    for char in s:
+        if char in string.punctuation:
+            return false
+
+        len(plate) <= 6 and len(char) >=2
+        plate[len(char)/2] != number
+        if char
+
+
+
+
+
+    ...
+
+
+main()
